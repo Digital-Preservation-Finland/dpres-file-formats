d13706e93e869c20affb4afb1559452ed949733c
Johan Kylander <johan.kylander@csc.fi>
2024-10-24 06:12:08 +0000

    PASMM-127 Add python functions to read file format registry

---
+++ dpres_file_formats/read_file_formats.py
@@ -0,0 +1,263 @@
+"""Functions that output the file formats list."""
+
+import json
+from dpres_file_formats.data.av_container_grading import CONTAINER_GRADES
+from dpres_file_formats.defaults import (FILE_FORMATS_JSON,
+                                         Grades)
+from dpres_file_formats.json_handler import FileFormatsJson
+
+
+def supported_file_formats(active=True, dps_spec_formats=True):
+    """Return supported file formats as a list of dicts.
+
+    :active: Returns only active formats and versions or not,
+        defaults to True
+    :dps_spec_formats: Returns only formats offically in the DPS
+        spec, discarding bit level and unoffically supported file
+        formats, defaults to True
+    :returns: A list of dicts
+    """
+    file_formats = FileFormatsJson().read_file_formats(
+        path=FILE_FORMATS_JSON)
+
+    formats = []
+
+    if active or dps_spec_formats:
+        for file_format in file_formats:
+
+            format_active = False
+            active_versions = []
+
+            # Set file format as active if any version is active or if
+            # the format is a DPS format
+            for version in file_format['versions']:
+                dps_format = any((
+                    dps_spec_formats and version['added_in_dps_spec'],
+                    not dps_spec_formats))
+                version_active = any((active and version['active'],
+                                      not active))
+                if version_active and dps_format:
+                    format_active = True
+                    active_versions.append(version)
+
+            # Include only active versions in the output
+            file_format['versions'] = active_versions
+            if format_active:
+                formats.append(file_format)
+
+    else:
+        formats = file_formats
+
+    return formats
+
+
+def supported_file_formats_versions(active=True,
+                                    dps_spec_formats=True,
+                                    basic_info=False):
+    """Return a list of flattened dicts of supported file format
+    with each version separately.
+
+    :active: Returns only active formats and versions or not,
+        defaults to True
+    :dps_spec_formats: Returns only formats offically in the DPS
+        spec, discarding bit level and unoffically supported file
+        formats, defaults to True
+    :basic_info: Returns only the mimetype and the version keys
+    :returns: A list of dicts
+    """
+    file_formats = FileFormatsJson().read_file_formats(
+        path=FILE_FORMATS_JSON)
+
+    format_versions = []
+
+    for file_format in file_formats:
+
+        if not basic_info:
+            format_dict = file_format.copy()
+            # Exclude some file format keys from the output
+            for key in ['versions', '_id']:
+                del format_dict[key]
+        else:
+            format_dict = {'mimetype': file_format['mimetype']}
+
+        # Merge file format and version dicts
+        for version in file_format['versions']:
+            version_dict = version.copy()
+            if basic_info:
+                version_dict = {'version': version['version']}
+            format_dict.update(version_dict)
+
+            # Set file format version as active if it is active or if
+            # it is a DPS format
+            dps_format = any((
+                dps_spec_formats and version['added_in_dps_spec'],
+                not dps_spec_formats))
+            version_active = any((active and version['active'],
+                                  not active))
+
+            # Add only active format to the output when active is True
+            if version_active and dps_format:
+                format_versions.append(format_dict)
+
+    return format_versions
+
+
+def mimetypes_grading(text_formats=False):
+    """Return a dictionary of file format gradings for active
+    file format versions.
+
+    The grading dictionary is based on mimetypes and their versions.
+    Each active file format with the same mimetype have their
+    active versions merged in the output.
+
+    :text_files: Return only text formats that require charset
+        information, defaults to false
+    :return: A dictionary of mimetypes with their graded versions
+    """
+    file_formats = FileFormatsJson().read_file_formats(
+        path=FILE_FORMATS_JSON)
+    mimetypes = {}
+
+    for file_format in file_formats:
+        format_versions = {}
+
+        # Populate mimetype dict with active versions only
+        for version in file_format['versions']:
+            if version['active']:
+                format_versions[version['version']] = version['grade']
+
+        # Add format only if it has active versions
+        if format_versions:
+
+            # Add only formats with charsets if text_formats is True
+            if any((not text_formats,
+                    text_formats and file_format['charsets'])):
+
+                # If mimetype has been added, just add the new versions
+                # to the previously added mimetyoe
+                if file_format['mimetype'] in mimetypes:
+                    mimetypes[file_format['mimetype']].update(format_versions)
+                else:
+                    mimetypes[file_format['mimetype']] = format_versions
+
+    return mimetypes
+
+
+def serialize_supported_file_formats(active=True, dps_spec_formats=True):
+    """Serialize supported file formats to a string.
+
+    :active: Returns only active formats and versions or not,
+        defaults to True
+    :dps_spec_formats: Returns only formats offically in the DPS
+        spec, discarding bit level and unoffically supported file
+        formats, defaults to True
+    :returns: A string of serialized JSON data with indentations
+    """
+    file_formats = {'file_formats': supported_file_formats(
+        active=active, dps_spec_formats=dps_spec_formats)}
+    return json.dumps(file_formats, indent=4, ensure_ascii=False)
+
+
+def serialize_supported_file_formats_versions(active=True,
+                                              dps_spec_formats=True,
+                                              basic_info=False):
+    """Serialize supported file format versions to a string.
+
+    :active: Returns only active formats and versions or not,
+        defaults to True
+    :dps_spec_formats: Returns only formats offically in the DPS
+        spec, discarding bit level and unoffically supported file
+        formats, defaults to True
+    :basic_info: Returns only the mimetype and the version keys
+    :returns: A string of serialized JSON data with indentations
+    """
+    file_formats = {
+        'file_formats': supported_file_formats_versions(
+            active=active,
+            dps_spec_formats=dps_spec_formats,
+            basic_info=basic_info)}
+    return json.dumps(file_formats, indent=4, ensure_ascii=False)
+
+
+def find_mimetypes(mimetype):
+    """Returns file formats from the file formats list based on
+    a given MIME type.
+
+    :mimetype: The MIME type as a string
+    :returns: The file formats as a list
+    """
+    file_formats = FileFormatsJson().read_file_formats(
+        path=FILE_FORMATS_JSON)
+    formats = []
+    for format_dict in file_formats:
+        if format_dict['mimetype'] == mimetype:
+            formats.append(format_dict)
+    return formats
+
+
+def mimetypes_format_registry_keys():
+    """Returns a dictionary of active mimetypes and their
+    corresponding format registry keys.
+
+    :returns: A dictionary of mimetypes with their format registry keys
+    """
+    file_formats = FileFormatsJson().read_file_formats(
+        path=FILE_FORMATS_JSON)
+    mimetypes = {}
+
+    for file_format in file_formats:
+        active = False
+        format_registry_keys = set()
+
+        # Populate format registry keys dict with data from active
+        # versions only
+        for version in file_format['versions']:
+            if version['active']:
+                active = True
+                format_registry_keys.add(version['format_registry_key'])
+
+        # Add format only if it has active versions
+        if active:
+
+            # If mimetype has been added, just add the new format
+            # registry keys to the previously added mimetyoe
+            if file_format['mimetype'] in mimetypes:
+                mimetypes[file_format['mimetype']].update(format_registry_keys)
+            else:
+                mimetypes[file_format['mimetype']] = format_registry_keys
+
+    return mimetypes
+
+
+def containers_streams_grading(grade=None):
+    """Returns a dictionary of AV containers with a grading and
+    supported bit streams as tuples.
+
+    :grade: The DPS grade for returning only a certain grade
+    :returns: A dictionary of AV containers
+    """
+    if grade:
+        grade = Grades[grade].value
+
+    containers = {}
+
+    for container_dict in CONTAINER_GRADES:
+
+        # Construct stream tuples
+        streams = set()
+        for stream in container_dict['audio_streams']:
+            streams.add((stream['mimetype'], stream['version']))
+        for stream in container_dict['video_streams']:
+            streams.add((stream['mimetype'], stream['version']))
+
+        if any((grade and container_dict['grade'] == grade,
+                not grade)):
+
+            # The mimetype is not a unique key when returning all
+            # containers regardless of grade
+            if container_dict['mimetype'] in containers:
+                containers[container_dict['mimetype']].update(streams)
+            else:
+                containers[container_dict['mimetype']] = streams
+
+    return containers
---
+++ tests/read_file_formats_test.py
@@ -0,0 +1,126 @@
+"""Unit tests for the read file formats module."""
+
+import pytest
+from dpres_file_formats.read_file_formats import (
+    containers_streams_grading,
+    find_mimetypes,
+    mimetypes_format_registry_keys,
+    mimetypes_grading,
+    serialize_supported_file_formats,
+    serialize_supported_file_formats_versions,
+    supported_file_formats,
+    supported_file_formats_versions
+)
+
+
+@pytest.mark.parametrize(
+    ("active", "dps_spec_formats", "found_formats", "found_versions"),
+    [
+        (True, True, 3, 1),
+        (False, True, 4, 2),
+        (True, False, 3, 2),
+        (False, False, 4, 3)
+    ]
+)
+def test_supported_file_formats(
+        active, dps_spec_formats, found_formats, found_versions):
+    """Test supported_file_formats."""
+    file_formats = supported_file_formats(active, dps_spec_formats)
+
+    assert len(file_formats) == found_formats
+    assert file_formats[0]["mimetype"]
+    assert len(file_formats[0]["versions"]) == found_versions
+    assert file_formats[0]["versions"][0]["version"]
+
+
+@pytest.mark.parametrize(
+    ("active", "dps_spec_formats", "basic_info", "found_formats"),
+    [
+        (True, True, False, 3),
+        (False, True, False, 5),
+        (True, False, False, 4),
+        (False, False, False, 6),
+        (True, True, True, 3)
+    ]
+)
+def test_supported_file_formats_versions(
+        active, dps_spec_formats, basic_info, found_formats):
+    """test supported_file_formats_versions."""
+    file_formats_versions = supported_file_formats_versions(
+            active=active,
+            dps_spec_formats=dps_spec_formats,
+            basic_info=basic_info)
+
+    assert len(file_formats_versions) == found_formats
+    assert file_formats_versions[0]["mimetype"]
+    assert file_formats_versions[0]["version"]
+    if basic_info:
+        assert len(file_formats_versions[0]) == 2
+
+
+def test_serialize_supported_file_formats():
+    """Test serialize_supported_file_formats."""
+    assert isinstance(serialize_supported_file_formats(), str)
+
+
+def test_serialize_supported_file_formats_versions():
+    """Test serialize_supported_file_formats_versions."""
+    assert isinstance(serialize_supported_file_formats_versions(), str)
+
+
+@pytest.mark.parametrize(("text_formats", "found_mimetypes"), [
+    (False, 2),
+    (True, 1)
+])
+def test_mimetypes_grading(text_formats, found_mimetypes):
+    """Test mimetypes_grading."""
+    mimetypes = mimetypes_grading(text_formats)
+    assert len(mimetypes) == found_mimetypes
+    assert "bbb/ccc" in mimetypes
+    if not text_formats:
+        assert "aaa/bbb" in mimetypes
+        assert mimetypes["aaa/bbb"]["2"] == (
+                "fi-dpres-recommended-file-format")
+        # Check that two formats with same mimetype have merged and
+        # that inactive versions have been excluded
+        assert len(mimetypes["aaa/bbb"]) == 3
+        assert ["2", "3", "5"] == list(mimetypes["aaa/bbb"].keys())
+        assert "1" not in mimetypes["aaa/bbb"]
+
+
+@pytest.mark.parametrize(("mimetype", "found"), [
+    ("aaa/bbb", 2),
+    ("unknown", 0)
+])
+def test_find_mimetypes(mimetype, found):
+    """Test find_mimetypes."""
+    formats = find_mimetypes(mimetype)
+    assert len(formats) == found
+
+
+def test_mimetypes_format_registry_keys():
+    """Test mimetypes_format_registry_keys."""
+    mimetypes = mimetypes_format_registry_keys()
+
+    # Two formats with the same mimetypes should have been combined
+    # and only the active versions' registry keys should have been added
+    assert len(mimetypes) == 2
+    assert len(mimetypes["aaa/bbb"]) == 2
+    for registry_keys in mimetypes["aaa/bbb"]:
+        assert registry_keys in ['key_002', 'key_004']
+
+    # Empty format registry keys should have been added
+    assert len(mimetypes["bbb/ccc"]) == 1
+    assert next(iter(mimetypes["bbb/ccc"])) == ''
+
+
+@pytest.mark.parametrize(("grade", "expected_mimetype", "found_count"), [
+    ("RECOMMENDED", "aaa/bbb", 1),
+    ("ACCEPTABLE", "bbb/ccc", 1),
+    (None, "aaa/bbb", 2)
+])
+def test_containers_streams_grading(grade, expected_mimetype, found_count):
+    """Test containers_streams_grading."""
+    containers = containers_streams_grading(grade=grade)
+    assert len(containers) == found_count
+    assert containers[expected_mimetype]
