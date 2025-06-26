9a7b1b87971f4f5888dcd56e29e1eea6250537c9
Eljas Veijalainen <eveijala@csc.fi>
2025-06-26 10:23:42 +0300

    TPASPKT-1414 Fix linting errors

--- dpres_file_formats/__init__.py
+++ dpres_file_formats/__init__.py
@@ -11,4 +11,9 @@ from dpres_file_formats.update_file_formats import (
     replace_format)
 from dpres_file_formats.graders import grade
 
-__all__ = ["file_formats", "av_container_grading", "add_format", "add_version_to_format", "replace_format", "grade"]
+__all__ = ["file_formats",
+           "av_container_grading",
+           "add_format",
+           "add_version_to_format",
+           "replace_format",
+           "grade"]
